733d7f88f57e79c878d68940246224b0eb0e2697
Helenapati <88396481+Helenapati@users.noreply.github.com>
2021-11-17 22:11:35 -0300

    Aluna: Helena Patrícia de Souza Silva
    Disciplina: Linguagem de Programação
    Data: 17/11/2021

---
+++ Main (6).java
@@ -0,0 +1,51 @@
+public class Questão 1{
+    
+    import java.util.List;
+    import java.util.ArrayList;
+    
+    public class Produto{
+        public static void main (String[] args){
+            
+            Flout mês1 = 5;
+            Flout mês2 = 6;
+            Flout mês3 = 6,50;
+            Flout mês4 = 5;
+            Flout mês5 = 5,50;
+            Flout mês6 = 6;
+            Flout mês7 = 7;
+            Flout mês8 = 6;
+            Flout mês9 = 5,50;
+            Flout mês10 = 5;
+            Flout mês11 = 4,50;
+            Flout mês12 = 5,50;
+            
+            ArrayList<Flout> mês = new ArrayList<> ();
+            mês.add(mês1);
+            mês.add(mês2);
+            mês.add(mês3);
+            mês.add(mês4);
+            mês.add(mês5);
+            mês.add(mês6);
+            mês.add(mês7);
+            mês.add(mês8);
+            mês.add(mês9);
+            mês.add(mês10);
+            mês.add(mês11);
+            mês.add(mês12);
+            
+            System.out.println(mês);
+            
+            Media = (mês1 + mês2 + mês3 + mês4 + mês5 + mês6 + mês7 + mês8 + mês9 + mês10 + mês11 + mês12) / 12 
+            
+            System.out.println(Media);
+            
+            
+        
+            
+            
+            
+            
+            
+        } 
+    }
+}
